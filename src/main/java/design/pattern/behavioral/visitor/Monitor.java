4f14d48b29112cfc6b302d2bb90497d3545f2821
Henry Ren <renxg126@126.com>
2019-11-14 16:54:11 +0800

    visitor

---
+++ src/main/java/design/pattern/behavioral/visitor/Monitor.java
@@ -0,0 +1,9 @@
+package design.pattern.behavioral.visitor;
+
+public class Monitor implements ComputerPart {
+
+	@Override
+	public void accept(ComputerPartVisitor computerPartVisitor) {
+		computerPartVisitor.visit(this);
+	}
+}
