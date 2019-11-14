89add1bc8962c36014178c7f4fb640f81724ea6c
Henry Ren <renxg126@126.com>
2019-11-14 16:52:16 +0800

    memento

---
+++ src/main/java/design/pattern/behavioral/memento/Memento.java
@@ -0,0 +1,13 @@
+package design.pattern.behavioral.memento;
+
+public class Memento {
+	private String state;
+
+	public Memento(String state) {
+		this.state = state;
+	}
+
+	public String getState() {
+		return state;
+	}
+}
